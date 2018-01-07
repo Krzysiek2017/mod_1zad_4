e4dcd200876a622053a0c84355fc7b7a0f52cfea
Krzysztof <musashimiyamoto@interia.eu>
2018-01-07 20:33:14 +0100

    adding

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿var fs = require('fs');
+var colors = require('colors');
+
+function fromFolderToFile(path, destination) {
+    
+    fs.readdir(path, 'utf-8', function(err, files) {
+        console.log('Odczyt: '.blue, files);
+
+        fs.writeFile(destination, files, (err) => {
+            if (err) throw err;
+            console.log('Plik został zapisany!'.blue, '\nTreść pliku: '.yellow, files);
+        });
+    });
+};
+fromFolderToFile('./folder', 'plik.txt');
