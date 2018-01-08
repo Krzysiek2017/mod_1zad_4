c1088c3c354e52a7231805485bb47c50ca3077e3
Krzysztof <musashimiyamoto@interia.eu>
2018-01-08 13:43:04 +0100

    poprawka

--- index.js
+++ index.js
@@ -2,15 +2,13 @@
 var colors = require('colors');
 
 function fromFolderToFile(path, destination) {
-    
     fs.readdir(path, 'utf-8', function(err, files) {
-               if (err) throw err;
+        if (err) throw err;
         console.log('Odczyt: '.blue, files);
-
-        fs.writeFile(destination, files, (err) => {
+        fs.writeFile(destination, files, function(err) {
             if (err) throw err;
             console.log('Plik został zapisany!'.blue, '\nTreść pliku: '.yellow, files);
         });
-});
+    });
 };
 fromFolderToFile('./folder', 'plik.txt');
