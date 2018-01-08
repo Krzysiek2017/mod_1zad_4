8b45bbe6f684d7cbb759de52a6005460053a4c21
Krzysztof <musashimiyamoto@interia.eu>
2018-01-08 08:54:48 +0100

    poprawki

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ var colors = require('colors');
 function fromFolderToFile(path, destination) {
     
     fs.readdir(path, 'utf-8', function(err, files) {
+               if (err) throw err;
         console.log('Odczyt: '.blue, files);
 
         fs.writeFile(destination, files, (err) => {
             if (err) throw err;
             console.log('Plik został zapisany!'.blue, '\nTreść pliku: '.yellow, files);
         });
-    });
+});
 };
 fromFolderToFile('./folder', 'plik.txt');
